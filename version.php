44b22964bc8523b09423d65f5411c783b9b2384c
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2022-08-09 11:59:36 +0200

    Version 0.0.3 (2022080900)

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release = 'v0.0.2';
+$plugin->release = 'v0.0.3';
 $plugin->version   = 2022080900;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051100;        // Requires this Moodle version.
 $plugin->component = 'paygw_mpay24';       // Full name of the plugin (used for diagnostics).
